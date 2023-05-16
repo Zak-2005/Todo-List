e1d1405131464ec6a0d6add68b0e4c2bb6500e97
Zak-2005 <zak.mob2005@gmail.com>
2023-05-16 18:22:47 -0500

    Starting from scratch, main page content

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import inbox from "./inbox";
-import test from "./projects"
-import {sidebar, header} from "./mainStructure"
-import "./styles.css";
-
-const body = document.querySelector('body');
-body.appendChild(header);
-body.appendChild(sidebar);
